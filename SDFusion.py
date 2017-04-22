6c46a5b33638a5edaa1353ec0623e796f4a26b00
asesselmann <anna.sesselmann@tum.de>
2017-04-22 13:40:11 +0200

    added doxygen documentation

--- SDFusion.py
+++ SDFusion.py
@@ -1,32 +1,60 @@
 import adsk.core
 import adsk.fusion
 import traceback
+import os.path
 import xml.etree.ElementTree as ET
 import math
 import xml.dom.minidom as DOM
 
+## @package SDFusion
+# This is an exporter for Autodesk Fusion 360 models to SDFormat.
+#
+# This can be loaded as an addon in Fusion 360.
+# It exports all rigid groups of the robot model as links
+# to STL and creates nodes in the SDF file for them.
+# It creates SDF nodes for all joints of the robot model.
+# Supported joint types are: "fixed", "revolute", and "ball".
+
 # Global variables
 design = None
-fileDir = ""
-modelName = ""
+fileDir = "C:/Users/techtalentsVR1/Documents/roboy/fusion/SDFusion"
+modelName = "Roboy" 
 rootOcc = None
 
-# Transforms Fusion 360 coordinate system to Gazebo cooridnate system.
-def gazeboMatrix(m):
+## Transforms a matrix from Fusion 360 to Gazebo.
+#
+# This transforms a matrix given in the Fusion 360 coordinate system
+# to one in the Gazebo cooridnate system.
+#
+# @param self a matrix given wrt the Fusion 360 coordinate system
+# @return the matrix wrt the Gazebo coordinate system
+def gazeboMatrix(self):
     matrix = adsk.core.Matrix3D.create()
     matrix.setCell(1, 1, 0)
     matrix.setCell(1, 2, -1)
     matrix.setCell(2, 1, 1)
     matrix.setCell(2, 2, 0)
-    m.transformBy(matrix)
-    return m
+    self.transformBy(matrix)
+    return self
 
-# Converts three double values to string.
+## Converts three double values to string.
+#
+# This function converts three double values to a string separated by spaces.
+#
+# @param x the first double value
+# @param y the second double value
+# @param z the third double value
+# @return the string of these values
 def vectorToString(x, y, z):
     string = str(x) + " " + str(y) + " " + str(z)
     return string
 
-# Builds SDF pose node from vector.
+## Builds SDF pose node from vector.
+#
+# This function builds the SDF pose node for every joint.
+#
+# @param vector the vector pointing to the origin of the joint.
+# @return the SDF pose node
 def sdfPoseVector(vector):
     pose = ET.Element("pose", frame="")
     # convert from cm (Fusion 360) to m (SI)
@@ -38,7 +66,12 @@ def sdfPoseVector(vector):
     pose.text = pos + " " + rot
     return pose
     
-# Builds SDF pose node from matrix.
+## Builds SDF pose node from matrix.
+#
+# This function builds the SDF pose node for every link.
+#
+# @param matrix the transformation matrix of the link
+# @return the SDF pose node
 def sdfPoseMatrix(matrix):
     pose = ET.Element("pose", frame="")
     # convert from cm (Fusion 360) to m (SI)
@@ -61,7 +94,12 @@ def sdfPoseMatrix(matrix):
     pose.text = pos + " " + rot
     return pose
 
-# Builds SDF inertial node from physical properties.
+## Builds SDF inertial node from physical properties.
+#
+# This function builds the SDF inertial node for every link.
+#
+# @param physics the physical properties of a link
+# @return the SDF inertial node
 def sdfInertial(physics):
     inertial = ET.Element("inertial")
     # build pose node of COM
@@ -77,14 +115,25 @@ def sdfInertial(physics):
     inertial.append(inertia)
     return inertial
 
-# Builds SDF node for one moment of inertia.
+## Builds SDF node for one moment of inertia.
+#
+# This helper function builds the SDF node for one moment of inertia.
+#
+# @param tag the tag of the XML node
+# @param value the text of the XML node
+# @return the SDF moment of inertia node
 def sdfMom(tag, value):
     node = ET.Element(tag)
     # convert from kg/cm^2 (Fusion 360) to kg/m^2 (SI)
     node.text = str(0.0001 * value)
     return node
 
-# Builds SDF inertia node from physical properties.
+## Builds SDF inertia node from physical properties.
+#
+# This function builds the SDF inertia node for every link.
+#
+# @param physics the physical properties of a link
+# @return the SDF inertia node
 def sdfInertia(physics):
     inertia = ET.Element("inertia")
     (returnValue, xx, yy, zz, xy, yz, xz) = physics.getXYZMomentsOfInertia()
@@ -96,7 +145,12 @@ def sdfInertia(physics):
     inertia.append(sdfMom("izz", zz))
     return inertia
     
-# Builds SDF link node.
+## Builds SDF link node.
+#
+# This function builds the SDF link node for every link.
+#
+# @param lin the link to be exported
+# @return the SDF link node
 def linkSDF(lin):
     linkName = lin.component.name
     link = ET.Element("link", name=linkName)
@@ -133,7 +187,14 @@ def linkSDF(lin):
     link.append(visual)
     return link
     
-# Builds SDF joint node.
+## Builds SDF joint node.
+#
+# This function builds the SDF joint node for every joint type.
+#
+# @param joi the joint 
+# @param name_parent the name of the parent link
+# @param name_child the name of the child link
+# @return the SDF joint node
 def jointSDF(joi, name_parent, name_child):
     jointInfo = []
     jointType = ""
@@ -174,7 +235,13 @@ def jointSDF(joi, name_parent, name_child):
     joint.extend(jointInfo)
     return joint
 
-# Builds SDF axis node for revolute joints.
+## Builds SDF axis node for revolute joints.
+#
+# This function builds the SDF axis node for revolute joint.
+#
+# @param joi one revolute joint object
+# @return a list of information nodes (here one axis node)
+# for the revolute joint
 def revoluteJoint(joi):
     info = []
     # build axis node
@@ -205,7 +272,10 @@ def revoluteJoint(joi):
     info.append(axis)
     return info
     
-# Plain STL export.
+## Plain STL export.
+##
+# @param occ the occurrence to be exported
+# @param linkName the name of the created STL file
 def exportToSTL(occ, linkName):
     global design
     global fileDir
@@ -214,7 +284,15 @@ def exportToSTL(occ, linkName):
     stlExportOptions = desExp.createSTLExportOptions(occ, fileName)
     desExp.execute(stlExportOptions)
 
-# Exports a rigid group to STL.
+## Exports a rigid group to STL.
+## Transforms a matrix from Fusion 360 to Gazebo.
+#
+# This exports a rigid group as one STL file.
+# For this all components of the rigidGroup are copied to a new component.
+#
+# @param rig the rigid group to be exported
+# @return a new occurrence which is used to export the
+# relevant information to SDFormat
 def rigidGroupToSTL(rig):
     global rootOcc
     linkName = rig.name
@@ -231,7 +309,13 @@ def rigidGroupToSTL(rig):
     exportToSTL(linkOcc, linkName)
     return linkOcc
     
-# Export an single occurrence to STL.
+## Exports an single occurrence to STL.
+#
+# This exports a single Fusion occurence as an STL file.
+#
+# @param occ the occurrence that needs to be exported.
+# @return a new occurrence which is used to export the
+# relevant information to SDFormat
 def occurrenceToSTL(occ):
     global rootOcc
     linkName = clearName(occ.name)
@@ -246,12 +330,19 @@ def occurrenceToSTL(occ):
     exportToSTL(linkOcc, linkName)
     return linkOcc
 
-# Clear filenames of unwanted characters
+## Clear filenames of unwanted characters
+#
+# This function replaces all ':' with underscores and deletes spaces in filenames.
+# to one in the Gazebo cooridnate system.
+#
+# @param name a filename
+# @return the filename without ':' and spaces
 def clearName(name):
     name = name.replace(":", "_")
     name = name.replace(" ", "")
     return name
-        
+
+## Exports a robot model from Fusion 360 to SDFormat.
 def run(context):
     ui = None
     try:
@@ -265,13 +356,7 @@ def run(context):
         rootComp = design.rootComponent
         # get all occurrences within the root component
         global rootOcc
-        rootOcc = rootComp.occurrences
-        # specify output location
-        global fileDir
-        fileDir = "C:/Users/techtalentsVR1/Documents/roboy/fusion/SDFusion"
-        # specify model name
-        global modelName
-        modelName = "Roboy"
+        rootOcc = rootComponent.occurrences
         # build sdf root node
         root = ET.Element("sdf", version="1.6")
         model = ET.Element("model", name=modelName)
